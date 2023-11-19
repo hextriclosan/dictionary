73e17c37667172eb9e9dd17003c989c2fb555842
Maksym Solomkin <maksym@solomk.in>
2023-11-19 12:34:00 -0500

    Fix tests and override conflicts

--- dictionary-client/src/components/app/App.test.js
+++ dictionary-client/src/components/app/App.test.js
@@ -1,8 +1,16 @@
 import {render, screen} from '@testing-library/react';
 import App from './App';
+import {BrowserRouter} from "react-router-dom";
+import {AuthProvider} from "../auth/authentication-helpers";
 
 test('renders dictionary header', () => {
-  render(<App/>);
-  const headerElement = screen.getByText(/Dictionary/i);
-  expect(headerElement).toBeInTheDocument();
+    render(
+        <BrowserRouter>
+            <AuthProvider>
+                <App/>
+            </AuthProvider>
+        </BrowserRouter>
+    );
+    const headerElement = screen.getByText(/Dictionary/i);
+    expect(headerElement).toBeInTheDocument();
 });
