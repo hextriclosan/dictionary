71ab172e7d94539911cbf1b35b35a22b6be5d5ff
Maksym Solomkin <maksym@solomk.in>
2023-11-26 23:39:18 -0500

    Support additional fields while adding word

--- dictionary-client/src/components/learning-items/AddLearningItemComponent.tsx
+++ dictionary-client/src/components/learning-items/AddLearningItemComponent.tsx
@@ -1,46 +1,19 @@
 import React, {useEffect, useState} from "react";
 import useCurrentLanguage from "../../context/CurrentLanguageContext";
 import {LearningItem} from "../../client/model/learning-item";
-import {Button} from "antd";
+import {Button, Form, Input, Space} from "antd";
 import {useDictionaryClient} from "../../client/learning-items/learning-items-client";
+import {MinusCircleOutlined, PlusOutlined} from "@ant-design/icons";
+import {UnsavedLearningItem} from "../../client/model/unsaved-learning-item";
 
 interface AddLearningItemComponentProps {
     onLearningItemAdded: (learningItem: LearningItem) => void
 }
 
 function AddLearningItemComponent(props: AddLearningItemComponentProps) {
-    const [text, setText] = useState("");
-    const [translation, setTranslation] = useState("");
     const [showAddLearningItemInput, setShowAddLearningItemInput] = useState(false);
     const currentLanguageContext = useCurrentLanguage();
-
     const client = useDictionaryClient();
-
-    function onAddLearningItem() {
-        console.log("Adding learningItem", currentLanguageContext, text, translation);
-        client
-            .addLearningItem(
-                currentLanguageContext.currentLanguage!,
-                {
-                    text: text,
-                    definitions: [
-                        {
-                            translation: translation
-                        }
-                    ]
-                }
-            )
-            .then((learningItem) => props.onLearningItemAdded(learningItem));
-        setShowAddLearningItemInput(false);
-    }
-
-    function handleKeyDown(event: React.KeyboardEvent<HTMLInputElement>) {
-        if (event.key === "Enter") {
-            onAddLearningItem();
-        } else if (event.key === "Escape") {
-            setShowAddLearningItemInput(false);
-        }
-    }
 
     useEffect(() => {
         function handleEscapeKey(event: KeyboardEvent) {
@@ -62,33 +35,115 @@ function AddLearningItemComponent(props: AddLearningItemComponentProps) {
         </Button>
     }
 
+    const onFinish = async (unsavedLearningItem: UnsavedLearningItem) => {
+        console.log("Adding learningItem from form", unsavedLearningItem, currentLanguageContext.currentLanguage);
+        const learningItem = await client
+            .addLearningItem(currentLanguageContext.currentLanguage!, unsavedLearningItem);
+        props.onLearningItemAdded(learningItem);
+        setShowAddLearningItemInput(false);
+    };
+
     return <div>
         <h1>Add learning item:</h1>
-        <div>
-            <label htmlFor="learningItem">Learning Item:</label>
-            <input
-                id="learningItem"
-                type="text"
-                value={text}
-                onChange={(event) => setText(event.target.value)}
-                onKeyDown={handleKeyDown}
-            />
-        </div>
-        <div>
-            <label htmlFor="translation">Translation:</label>
-            <input
-                id="translation"
-                type="text"
-                value={translation}
-                onChange={(event) => setTranslation(event.target.value)}
-                onKeyDown={handleKeyDown}
-            />
-        </div>
-        <div>
-            <Button onClick={onAddLearningItem} type="primary" style={{marginBottom: 16}}>Add Learning Item</Button>
-            <Button onClick={() => setShowAddLearningItemInput(false)} type="default"
-                    style={{marginBottom: 16}}>Cancel</Button>
-        </div>
+
+        <Form
+            name="learning-item"
+            onFinish={onFinish}
+            style={{maxWidth: 600}}
+            autoComplete="off"
+            initialValues={{
+                definitions: [
+                    {
+                        definition: "",
+                        translation: "",
+                        comment: ""
+                    }
+                ]
+            }}
+        >
+            <Form.Item
+                label="Text"
+                name="text"
+                required={true}
+            >
+                <Input placeholder="Learning Item Text"/>
+            </Form.Item>
+            <Form.Item
+                label="Comment"
+                name="comment"
+            >
+                <Input placeholder="Learning Item Comment"/>
+            </Form.Item>
+            <Form.Item
+                label="Image URL"
+                name="imageUrl"
+                rules={[{type: 'url', message: 'Please enter a valid URL'}]}
+            >
+                <Input placeholder="http://example.com/image.png"/>
+            </Form.Item>
+            <Form.List name="definitions"
+                       rules={[
+                           {
+                               validator: async (_, definitions) => {
+                                   definitions.forEach((definition: any) => {
+                                       if (!definition.definition && !definition.translation) {
+                                           return Promise.reject(new Error('Learning item should contain either definition or translation'));
+                                       }
+                                   });
+                               },
+                               message: 'Learning item should contain either definition or translation'
+                           },
+                       ]}
+            >
+                {(fields, {add, remove}, {errors}) => (
+                    <>
+                        {fields.map(({key, name, ...restField}) => (
+                            <Space key={key} style={{display: 'flex', marginBottom: 8}} align="baseline">
+                                <Form.Item
+                                    {...restField}
+                                    name={[name, 'definition']}
+                                >
+                                    <Input placeholder="Define item"/>
+                                </Form.Item>
+                                <Form.Item
+                                    {...restField}
+                                    name={[name, 'translation']}
+                                >
+                                    <Input placeholder="Translation"/>
+                                </Form.Item>
+                                <Form.Item
+                                    {...restField}
+                                    name={[name, 'comment']}
+                                >
+                                    <Input placeholder="Definition comment"/>
+                                </Form.Item>
+                                {fields.length > 1 ? (
+                                    <MinusCircleOutlined
+                                        // className="dynamic-delete-button"
+                                        onClick={() => remove(name)}
+                                    />
+                                ) : null}
+                                {/*<MinusCircleOutlined onClick={() => remove(name)}/>*/}
+                            </Space>
+                        ))}
+                        <Form.Item>
+                            <Button type="dashed" onClick={() => add()} block icon={<PlusOutlined/>}>
+                                Add field
+                            </Button>
+                        </Form.Item>
+                        <Form.ErrorList errors={errors}/>
+                    </>
+                )}
+            </Form.List>
+            <Form.Item>
+                <Space>
+                    <Button type="primary" htmlType="submit">
+                        Save
+                    </Button>
+                    <Button onClick={() => setShowAddLearningItemInput(false)} type="default">Cancel</Button>
+                </Space>
+            </Form.Item>
+        </Form>
     </div>
 }
 
